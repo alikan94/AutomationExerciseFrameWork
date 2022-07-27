ccf5ba5f9db542e5b93cbeda05615a1ea8b69c69
Alikan <dralikan94@gmail.com>
2022-07-27 21:21:44 +0300

    pomda bazı karışıklıklar oldu

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,18 +1,23 @@
 package tests;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 import pages.HomePage;
 import com.github.javafaker.Faker;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import pages.ProductsPage;
 import pages.SingleProductPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.TestBaseRapor;
 
+import static org.testng.Assert.assertTrue;
+
 public class BaseTest extends TestBaseRapor {
+
+    //C:\Program Files\apache-maven-3.8.6\bin
 
     HomePage homePage = new HomePage();
     ProductsPage productsPage = new ProductsPage();
@@ -21,18 +26,17 @@ public class BaseTest extends TestBaseRapor {
     SoftAssert softAssert = new SoftAssert();
 
 
-    @BeforeClass
+    //@BeforeClass
     public void navigateToHomePage() {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
         //3. Verify that home page is visible successfully
         Driver.getDriver().get(ConfigReader.getProperty("aeUrl"));
-        softAssert.assertTrue(homePage.logo.isDisplayed());
+        //assertTrue(homePage.logo.isDisplayed());
     }
 
-    @AfterClass
+    //@AfterClass
     public void tearDown() {
-        softAssert.assertAll();
         Driver.closeDriver();
     }
 
